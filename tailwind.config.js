8628ff4fb1b9c84a041494ea1b9402155f56d6ea
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-07-24 03:09:57 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        "light-coffee": "#C89F94",
+        'coffee': {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838",
+        }
+      },
+      screens: {
+        'xs': '480px',
+        ...defaultTheme.screens
       },
       fontFamily: {
         Karla: ["Karla", "sans-serif"],
@@ -14,10 +25,18 @@ module.exports = {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0)" },
         },
+        fadeIn: {
+          from: {opacity: 0},
+          to: {opacity :1}
+        }
       },
       animation: {
-        slideDown: "slideDown 0.3s ease-in-out",
+        slideDown: "slideDown 0.5s ease-in-out",
+        fadeIn: "fadeIn 0.5s ease-in-out",
       },
+      backgroundImage: {
+        'slider-bg': 'url("./img/slider-bg.jpg")'
+      }
     },
   },
   plugins: [],
